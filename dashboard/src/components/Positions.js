f88aefc1f839eb247ce93dd90634aaf978bdde65
patilmayurviks <patilmayurvikas@gmail.com>
2026-06-30 01:20:42 +0530

    update bakend url

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -8,7 +8,7 @@ const Positions = () => {
    const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allPositions").then((res) => {
+    axios.get("https://zerodha-clone-2-p3wc.onrender.com/allPositions").then((res) => {
       // console.log(res.data);
       setAllPositions(res.data);
     });
--- dashboard/src/components/SellActionWindow.js
+++ dashboard/src/components/SellActionWindow.js
@@ -14,7 +14,7 @@ const SellActionWindow = ({ uid }) => {
 
   const handleSellClick = async () => {
     try {
-      await axios.post("http://localhost:3002/newOrder", {
+      await axios.post("https://zerodha-clone-2-p3wc.onrender.com/newOrder", {
         name: uid,
         qty: stockQuantity,
         price: stockPrice,
